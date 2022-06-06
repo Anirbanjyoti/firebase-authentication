2b9b7de8e6c62ed09c90de9797f9fb9e2bf02848
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-06-06 16:30:05 +0530

    firebase-authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import app from './firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from 'react';
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+  const provider = new GoogleAuthProvider();
+  
+  const handleGoogleSignIn= () => {
+    signInWithPopup(auth, provider)
+    .then(result =>{
+        // The signed-in user info.
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+    })
+  }
+  const handleGoogleSignOut= () => {
+    signOut(auth)
+    .then(() => {
+      setUser({});
+      console.log(`Sign Out successful`);
+      
+    }).catch((error) => {
+      console.log('Error', error);
+    });
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google sign-in</button>
+      <button onClick={handleGoogleSignOut}>Google sign-Out</button>
+      <p><img src={user.photoURL}></img></p>
+      <p>User Name: {user.displayName}</p>
+      <p>User Email: {user.email}</p>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwu_cYzv5kQlOIHrpO-KBPHbfONUXdipg",
+  authDomain: "fir-authentication-5290f.firebaseapp.com",
+  projectId: "fir-authentication-5290f",
+  storageBucket: "fir-authentication-5290f.appspot.com",
+  messagingSenderId: "477677141215",
+  appId: "1:477677141215:web:8ebb54f9d3b7e77772bd6a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
